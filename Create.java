005150f8c0f24fa2578073140038039b71a5277b
ganeshteki <ganeshteki2001@gmail.com>
2024-08-12 20:46:26 +0530

    stack

--- Create.java
+++ Create.java
@@ -1,7 +1,20 @@
+
+import java.util.Iterator;
+import java.util.Stack;
+
 public class Create{
 public static void main(String[]args){
 // Create an Object for Stack
+Stack s = new Stack<>();
   // Load Values in Stack
+  s.push(10);
+  s.push(20);
+  s.push(30);
+
   // Iterate it by usig Iterator and While Loops
+  Iterator iterator = s.iterator();
+  while (iterator.hasNext()) {
+    System.out.println(iterator.next());
+  }
 }
 }
--- Enumerations.java
+++ Enumerations.java
@@ -1,8 +1,43 @@
+import java.util.Stack;
+import java.util.Enumeration;
+
 public class Enumerations{
-  public static void main(String[]args){
+  
+
    // Create an Object for Stack
   // Load person class Objects in it
   // Iterate it by using Enumeration
+
   // print the values by using while Loop
 
-  }}
+  
+
+
+    public static void main(String[] args) {
+
+        Stack<Person> s = new Stack<>();
+
+        s.push(new Person("pavani", 22));
+        s.push(new Person("ganesh", 22));
+        s.push(new Person("ramya", 18));
+
+        Enumeration<Person> enumeration = s.elements();
+
+        while (enumeration.hasMoreElements()) {
+            System.out.println(enumeration.nextElement());
+        }
+    }
+  }
+    class Person {
+      String name;
+      int age;
+
+      Person(String name, int age) {
+          this.name = name;
+          this.age = age;
+      }
+
+      public String toString() {
+          return name + "  " + age;
+      }
+  }
